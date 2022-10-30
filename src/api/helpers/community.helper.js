6400f54a27b58037f6858c0f583dbfe6f7d6d3ec
Mian Jazib Ali <m.mianjazibali@gmail.com>
2022-10-30 17:14:22 +0100

    Remove UpdatedAt From Fetch All Communities API

--- src/api/helpers/community.helper.js
+++ src/api/helpers/community.helper.js
@@ -2,7 +2,7 @@ const Community = require('../models/community.model');
 
 // eslint-disable-next-line arrow-body-style
 const fetchAllCommunities = ({ page = 1, perPage = 30, name = '' } = {}) => {
-  return Community.find({ name: { $regex: name } }, { users: 0 })
+  return Community.find({ name: { $regex: name } }, { users: 0, updatedAt: 0 })
     .sort({ name: 1 })
     .skip((page - 1) * perPage)
     .limit(perPage)
